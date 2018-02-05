20183a4d93f1647f1195331cd0d2f153a95f39a6
jakub <gryglos@gmail.com>
2018-02-05 14:08:38 +0100

    Relocating master loop from view

--- src/main/java/pl/kubie/catalogue/GameRepository.java
+++ src/main/java/pl/kubie/catalogue/GameRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 public class GameRepository {
+
     private Map<String,Game> gameList;
 
     public GameRepository() {
--- src/main/java/pl/kubie/catalogue/View.java
+++ src/main/java/pl/kubie/catalogue/View.java
@@ -1,117 +1,76 @@
 package pl.kubie.catalogue;
 
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class View {
-    static GameRepository gameRepository = new GameRepository();
-
-    static Game game1 = new Game("Wiedźmin", "Przygodowe", "Dobra gra", 10.0);
-    static Game game2 = new Game("Assassin's Creed", "Przygodowe ", "Dobra gra", 9.4);
-    static Game game3 = new Game("Gothic", "Przygodowe", "Dobra gra", 9.0);
-    static Game game4 = new Game("Need for speed", "Wyścigi", "Dobra gra", 8.0);
-    static Game game5 = new Game("GTA", "Przygodowe", "Dobra gra", 7.0);
 
     static Scanner scanner = new Scanner(System.in);
 
-    public static void executeProgramm() {
-        gameRepository.save(game1);
-        gameRepository.save(game2);
-        gameRepository.save(game3);
-        gameRepository.save(game4);
-        gameRepository.save(game5);
-        int choice = 1;
-
-        printMenu();
-        while (choice != 0) {
-            System.out.println("Choose option from menu");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 0: {
-                    break;
-                }
-                case 1: {
-                    printGames(gameRepository.getGameList());
-                    break;
-                }
-                case 2: {
-                    scanner.nextLine();
-                    System.out.println("Type in name:");
-                    String name = scanner.nextLine();
-                    System.out.println("Type in type:");
-                    String type = scanner.nextLine();
-                    System.out.println("Type in comment:");
-                    String comment = scanner.nextLine();
-                    System.out.println("Type in rate");
-                    double rate = scanner.nextInt();
-                    Game newGame = new Game(name, type, comment, rate);
-                    gameRepository.save(newGame);
-                    break;
-                }
-                case 3: {
-                    scanner.nextLine();
-                    System.out.println("Type in name game to delete");
-                    String name = scanner.nextLine();
-                    gameRepository.delete(name);
-                    break;
-                }
-                case 4: {
-                    scanner.nextLine();
-                    System.out.println("Type in name Game to edit");
-                    String oldName = scanner.nextLine();
-                    System.out.println("Type in new name");
-                    String newName = scanner.nextLine();
-                    System.out.println("Type in new type");
-                    String newType = scanner.nextLine();
-                    System.out.println("Type in new comment");
-                    String newComment = scanner.nextLine();
-                    System.out.println("Type in rate");
-                    double newRate = scanner.nextDouble();
-                    gameRepository.editGame(oldName,newName,newType,newComment,newRate);
-                    break;
-                }
-                case 5: {
-                    scanner.nextLine();
-                    System.out.println("Press 1 to find by name");
-                    System.out.println("Press 2 to find by rate");
-                    System.out.println("Press 3 to find by date");
-                    int findingChoice =scanner.nextInt();
-                    switch (findingChoice){
-                        case 1: {
-                            scanner.nextLine();
-                            System.out.println("Type in name");
-                            String name = scanner.nextLine();
-                            System.out.println();
-                            printGame(gameRepository.findByName(name));
-                            break;
-                        }
-                        case 2: {
-                            scanner.nextLine();
-                            System.out.println("Type in rate");
-                            double rate = scanner.nextDouble();
-                            List<Game> games = gameRepository.findByRate(rate);
-                            printGames(games);
-                            break;
-                        }
-                        case 3: {
-                            scanner.nextLine();
-                            System.out.println("Type in year");
-                            int year  = scanner.nextInt();
-                            System.out.println("Type in month");
-                            int month = scanner.nextInt();
-                            System.out.println("Type in day");
-                            int day = scanner.nextInt();
-                            LocalDate date = LocalDate.of(year,month,day);
-                            List<Game> games = gameRepository.findByDate(date);
-                            printGames(games);
-                            break;
-                        }
-                    }
-
-                }
-            }
-        }
+    public static Game addGame(){
+        scanner.nextLine();
+        System.out.println("Type in name:");
+        String name = scanner.nextLine();
+        System.out.println("Type in type:");
+        String type = scanner.nextLine();
+        System.out.println("Type in comment:");
+        String comment = scanner.nextLine();
+        System.out.println("Type in rate");
+        double rate = scanner.nextInt();
+        Game newGame = new Game(name, type, comment, rate);
+        return newGame;
+    }
+    public static String deleteGame(){
+        scanner.nextLine();
+        System.out.println("Type in name game to delete");
+        String name = scanner.nextLine();
+        return name;
+    }
+    public static List<String> editGame(){
+        List<String> values= new LinkedList<>();
+        scanner.nextLine();
+        System.out.println("Type in name Game to edit");
+        String oldName = scanner.nextLine();
+        System.out.println("Type in new name");
+        String newName = scanner.nextLine();
+        System.out.println("Type in new type");
+        String newType = scanner.nextLine();
+        System.out.println("Type in new comment");
+        String newComment = scanner.nextLine();
+        System.out.println("Type in rate");
+        String newRate = scanner.nextLine();
+        values.add(oldName);
+        values.add(newName);
+        values.add(newType);
+        values.add(newComment);
+        values.add(newRate);
+        return values;
+    }
+    public String findByName(){
+        scanner.nextLine();
+        System.out.println("Type in name");
+        String name = scanner.nextLine();
+        System.out.println();
+        return name;
+    }
+    public double findByRate(){
+        scanner.nextLine();
+        System.out.println("Type in rate");
+        double rate = scanner.nextDouble();
+        return rate;
+    }
+    public LocalDate findByDate(){
+        scanner.nextLine();
+        System.out.println("Type in year");
+        int year  = scanner.nextInt();
+        System.out.println("Type in month");
+        int month = scanner.nextInt();
+        System.out.println("Type in day");
+        int day = scanner.nextInt();
+        LocalDate date = LocalDate.of(year,month,day);
+        return date;
     }
 
     public static void printMenu() {
@@ -140,5 +99,6 @@ public class View {
         System.out.print("||");
         System.out.print(game.getDate());
     }
+
 
 }
